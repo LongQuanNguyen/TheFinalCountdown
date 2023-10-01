8f12918855daee3b61d0568ba171c61818e326f4
Long Quan Nguyen <imq026@usask.ca>
2023-10-01 16:33:41 -0600

    Get everything up and working, to-do: improve UI

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // Nguyen, Long Quan - iqm026 - 11314875
+
+const countDownDate = new Date("2023-12-09T23:59:59").getTime();
+
+// Update the countdown every 1 second
+const x = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    const timerElement = document.getElementById("timer");
+    timerElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+    // If the countdown is over, display a message
+    if (distance < 0) {
+        clearInterval(x);
+        timerElement.innerHTML = "EXPIRED";
+    }
+}, 1000);
