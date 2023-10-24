7c27e41ff9674f32a54a1871417a86921b055e86
OvercookedPotato <94843824+LongQuanNguyen@users.noreply.github.com>
2023-10-24 13:08:52 -0600

    UI completed implementing

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 // Nguyen, Long Quan - iqm026 - 11314875
 
-const countDownDate = new Date("2023-12-09T23:59:59").getTime();
+// Referencing: https://www.w3schools.com/howto/howto_js_countdown.asp
 
-// Update the countdown every 1 second
-const x = setInterval(function() {
-    const now = new Date().getTime();
+// This function return current Saskatchewan time
+function getCurrentTimeInSaskatchewan() {
+    // Saskatchewan's standard offset is UTC-6
+    const saskatchewanTimezoneOffset = -6 * 60;
+    // Check if it's currently daylight saving time (CDT)
+    const now = new Date();
+    const isDaylightSavingTime = now.getTimezoneOffset() === saskatchewanTimezoneOffset - 60;
+    // Calculate the offset for daylight saving time
+    const daylightSavingOffset = isDaylightSavingTime ? -60 : 0;  
+    // Calculate the total offset
+    const totalOffset = saskatchewanTimezoneOffset + daylightSavingOffset;
+    // Get the current time in Saskatchewan
+    const saskatchewanTime = new Date(now.getTime() + totalOffset * 60 * 1000);
+  
+    return saskatchewanTime;
+  }
+
+// This function do the countdown and display message when countdown is reach
+function countdown() {
+    const now = getCurrentTimeInSaskatchewan();
     const distance = countDownDate - now;
 
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
@@ -12,12 +29,26 @@ const x = setInterval(function() {
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    const timerElement = document.getElementById("timer");
-    timerElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    // Update the content of each div
+    document.getElementById("days").innerHTML = days + " D";
+    document.getElementById("hours").innerHTML = hours + " H";
+    document.getElementById("minutes").innerHTML = minutes + " M";
+    document.getElementById("seconds").innerHTML = seconds + " S";
 
     // If the countdown is over, display a message
     if (distance < 0) {
         clearInterval(x);
-        timerElement.innerHTML = "EXPIRED";
+        document.getElementById("days").innerHTML = 0 + " D";
+        document.getElementById("hours").innerHTML = 0 + " H";
+        document.getElementById("minutes").innerHTML = 0 + " M";
+        document.getElementById("seconds").innerHTML = 0 + " S";
+
+        document.getElementById("message-timeout").innerHTML = "AHH S***, HERE WE GO AGAIN!!";
     }
-}, 1000);
+}
+
+// Get the finals day (yeah i meant final final date)
+const countDownDate = new Date("2023-12-09T23:59:59").getTime();
+
+// Update the countdown every 1 second
+const x = setInterval(countdown, 1000);
